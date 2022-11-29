a1498884caa6104661927458036c000f03d742d6
MisterRuby <ruby8700@naver.com>
2022-11-30 00:22:48 +0900

    feat : ProviderId 에 해당하는 유저 검색 추가

--- src/domain/user/user.repository.ts
+++ src/domain/user/user.repository.ts
@@ -1,6 +1,10 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import { CustomRepository } from '../../module/typeorm/typeorm.decorator';
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import { CustomRepository } from "../../module/typeorm/typeorm.decorator";
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  findByProviderId(providerId: string): Promise<User | null> {
+    return this.findOneBy({ providerId });
+  }
+}
