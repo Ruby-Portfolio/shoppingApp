4dded5700bf773275bdbee60534a1f514dc83b04
MisterRuby <ruby8700@naver.com>
2022-12-01 18:09:59 +0900

    test : 마켓 정보 캐시 조회 단위 테스트
    캐시된 마켓 정보가 없을 경우 DB 에서 조회
    캐시된 마켓 정보가 있을 경우 캐시에서 조회

---
+++ test/domain/market/market.cache.spec.ts
@@ -0,0 +1,115 @@
+import { CACHE_MANAGER, CacheModule } from '@nestjs/common';
+import { User } from '../../../src/domain/user/user.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomTypeOrmModule } from '../../../src/module/typeorm/typeorm.module';
+import { UserRepository } from '../../../src/domain/user/user.repository';
+import { MarketRepository } from '../../../src/domain/market/market.repository';
+import { Market } from '../../../src/domain/market/market.entity';
+import { Cache } from 'cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
+import { MarketCache } from '../../../src/domain/market/market.cache';
+
+describe('MarketCache', () => {
+  let marketRepository: MarketRepository;
+  let cacheManager: Cache;
+  let marketCache: MarketCache;
+  let user: User;
+  let market: Market;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          entities: [User, Market],
+          charset: 'utf8mb4',
+          synchronize: true,
+          logging: true,
+        }),
+        CacheModule.register({
+          store: redisStore,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+          ttl: +process.env.REDIS_TTL,
+          isGlobal: true,
+        }),
+        CustomTypeOrmModule.forCustomRepository([
+          UserRepository,
+          MarketRepository,
+        ]),
+      ],
+      providers: [MarketCache],
+    }).compile();
+
+    const userRepository = module.get<UserRepository>(UserRepository);
+    marketRepository = module.get<MarketRepository>(MarketRepository);
+    cacheManager = module.get(CACHE_MANAGER);
+    marketCache = module.get<MarketCache>(MarketCache);
+
+    await marketRepository.delete({});
+    await userRepository.delete({});
+    user = await userRepository.save({
+      name: 'ruby',
+      email: 'ruby@gmail.com',
+      provider: 'google',
+      providerId: '124312',
+    });
+    market = await marketRepository.save({
+      name: '루비 상점',
+      description: '루비의 상점입니다.',
+      userId: user.id,
+    });
+  });
+
+  describe('getMarketCache - 캐시된 마켓 정보 조회', () => {
+    describe('캐시된 마켓 정보가 없을 경우', () => {
+      beforeAll(async () => {
+        await cacheManager.reset();
+      });
+
+      test('캐시된 마켓 정보가 없을 경우 DB 에서 조회. 조회된 데이터를 캐싱', async () => {
+        const findMarket = await marketCache.getMarketCache(market.id, user.id);
+        const cacheMarket = (await cacheManager.get(
+          `market_${market.id}_${user.id}`,
+        )) as Market;
+
+        expect(findMarket).toEqual(market);
+        expect(cacheMarket.id).toEqual(market.id);
+        expect(cacheMarket.description).toEqual(market.description);
+        expect(cacheMarket.name).toEqual(market.name);
+        expect(cacheMarket.userId).toEqual(market.userId);
+      });
+    });
+    describe('캐시된 마켓 정보가 있을 경우', () => {
+      beforeAll(async () => {
+        await cacheManager.set(`market_${market.id}_${user.id}`, market);
+      });
+
+      test('캐시된 마켓 정보가 있을 경우 캐시에서 조회', async () => {
+        jest
+          .spyOn(marketRepository, 'findOneBy')
+          .mockResolvedValue(Promise.resolve(new Market()));
+
+        const cacheMarket = await marketCache.getMarketCache(
+          market.id,
+          user.id,
+        );
+
+        expect(cacheMarket.id).toEqual(market.id);
+        expect(cacheMarket.description).toEqual(market.description);
+        expect(cacheMarket.name).toEqual(market.name);
+        expect(cacheMarket.userId).toEqual(market.userId);
+      });
+    });
+  });
+});
