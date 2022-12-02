21c5f19d13b37ccaec0c65e6539ccc86c02458da
MisterRuby <ruby8700@naver.com>
2022-12-03 00:36:08 +0900

    feat : 상품 정보 상세 조회 쿼리 구현

--- src/domain/product/product.repository.ts
+++ src/domain/product/product.repository.ts
@@ -1,6 +1,26 @@
 import { Repository } from 'typeorm';
 import { CustomRepository } from '../../module/typeorm/typeorm.decorator';
 import { Product } from './product.entity';
+import { ProductDetailDto } from './product.dto';
+import { Market } from '../market/market.entity';
+import { User } from '../user/user.entity';
 
 @CustomRepository(Product)
-export class ProductRepository extends Repository<Product> {}
+export class ProductRepository extends Repository<Product> {
+  async getProductDetail(productId: number): Promise<ProductDetailDto> {
+    return this.createQueryBuilder('product')
+      .leftJoinAndSelect(Market, 'market', 'market.id = product.marketId')
+      .leftJoinAndSelect(User, 'user', 'user.id = market.userId')
+      .select([
+        'product.id as id',
+        'product.name as name',
+        'product.price as price',
+        'product.stock as stock',
+        'product.description as description',
+        'market.name as marketName',
+        'user.name as userName',
+      ])
+      .where({ id: productId })
+      .getRawOne();
+  }
+}
