c3957147c1a020e16e701da5c754f1a095a17ffd
MisterRuby <ruby8700@naver.com>
2022-12-04 18:48:50 +0900

    feat : 주문 아이템 모듈 구현

---
+++ src/domain/orderItem/orderItem.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { DateColumns } from '../embedded/dateColumns';
+import { POrder } from '../pOrder/pOrder.entity';
+import { Product } from '../product/product.entity';
+
+@Entity()
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  pOrderId: number;
+
+  @Column()
+  productId: number;
+
+  @Column(() => DateColumns, { prefix: false })
+  dateColumns: DateColumns;
+
+  @ManyToOne(() => POrder)
+  @JoinColumn({ name: 'pOrderId' })
+  pOrder: POrder;
+
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'productId' })
+  product: Product;
+}
